b6c6fae8d7756e6ce50caa41ced2249f0ae0e3de
Humayoun Baig <humayoun.mani@gmail.com>
2020-04-25 10:17:19 +0500

    type: style
    body: added separate style component

---
+++ src/styles/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input: {
+        height: 40,
+        borderWidth: 1,
+        borderColor: "#dedede"
+    },
+    heading: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: "green",
+        padding: 10,
+        width: 150,
+        alignItems: 'center',
+        alignSelf: "center",
+        marginTop: 40,
+        borderRadius: 5
+
+    },
+    text: {
+        color: 'white'
+    }
+  });
