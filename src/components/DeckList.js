a37f0c96959518bea1cf8729a2b2b7c29020c1cf
Humayoun Baig <humayoun.mani@gmail.com>
2020-04-25 22:31:27 +0500

    type: feat
    body: integrated data directly from storage

--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -1,47 +1,55 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, Text, StyleSheet, SafeAreaView, FlatList, AsyncStorage, TouchableOpacity } from 'react-native';
 import {styles} from '../styles/styles'
-
+import { getInitialData } from '../utils/dataHandler'
 
 export default function DeckList(props) {
-  const DATA = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Item',
-      count: 3
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Item',
-      count: 1
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Item',
-      count: 2
-    },
-  ];
+  const [deckData, setDeckData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData()
+    
+  }, []);
+
+  const fetchData = async() =>{
+    setLoading(true)
+    const data = await getInitialData();
+    setDeckData(data);
+    setLoading(false)
+  }
+
+
+  function RenderItem({ item }) {
+
+    const {title, count} = item 
+    return (
+      <TouchableOpacity style={styles.listItem} onPress={()=> props.navigation.navigate("deckDetails", {deck: item})}>
+        <Text style={styles.heading}>{title}</Text>
+        <Text style={[styles.mutedText, {marginTop: 5}]}>{count} cards</Text>
+      </TouchableOpacity>
+    );
+  }
 
   return (
       <View style={styles.container}>
           <SafeAreaView>
-          <FlatList
-            data={DATA}
-            renderItem={({item})=> <RenderItem item={item}/>}
-            keyExtractor={item => item.id}
-          />
+          {
+            deckData &&
+            <FlatList
+              data={Object.values(deckData)}
+              renderItem={({item})=> <RenderItem item={item}/>}
+              keyExtractor={item => item.id}
+              onRefresh={fetchData}
+              refreshing={loading}
+              
+            />
+          }
+          
       
           </SafeAreaView>
       </View>
   );
 } 
 
-function RenderItem({ item }) {
-  const {title, count} = item 
-  return (
-    <TouchableOpacity style={styles.listItem} onPress={()=> props.navigation.navigate("deckDetails", {deck: item})}>
-      <Text style={styles.heading}>{title}</Text>
-      <Text style={[styles.mutedText, {marginTop: 5}]}>{count} cards</Text>
-    </TouchableOpacity>
-  );
-}
+
