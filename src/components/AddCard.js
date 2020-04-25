c8008659962cfc31c05738ff7888498470c1d5e3
Humayoun Baig <humayoun.mani@gmail.com>
2020-04-25 18:45:08 +0500

    type: feat
    body: added add card screen

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { View, Text, SafeAreaView, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
-import {styles} from '../styles/styles';
+import {styles, colors} from '../styles/styles';
 
 export default function AddCard() {
     return (
         <View style={styles.container}>
             <SafeAreaView>
-                <KeyboardAvoidingView>
-                    <Text style={[styles.heading, {marginBottom: 40}]}>What is the title of your new deck?</Text>
-                    <TextInput style={styles.input} placeholder="Deck Title" />
-                    <TouchableOpacity style={[styles.button, {backgroundColor: '#8bc34a', marginTop: 40}]}>
-                        <Text style={styles.text}>Create Deck</Text>
-                    </TouchableOpacity>
+                <KeyboardAvoidingView behavior="padding">
+                    <View style={{height: "100%", justifyContent: 'center'}}>
+                        <Text style={[styles.heading, {marginBottom: 40}]}>Please enter following to add new card</Text>
+                        <TextInput style={styles.input} placeholder="Enter Question" />
+                        <TextInput style={styles.input} placeholder="Enter Answer" />
+                    
+                        <TouchableOpacity style={[styles.button, {backgroundColor: colors.blue, marginTop: 40}]}>
+                            <Text style={styles.text}>Submit</Text>
+                        </TouchableOpacity>
+                    </View>
+                    
                 </KeyboardAvoidingView>
 
             </SafeAreaView>
