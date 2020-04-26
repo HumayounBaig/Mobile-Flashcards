53b15fa7d634dc23b6fa12fa655f3eba13d22c50
Humayoun Baig <humayoun.mani@gmail.com>
2020-04-26 17:01:52 +0500

    type style
    body: quiz styling

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3,7 +3,10 @@ import { StyleSheet, Dimensions } from 'react-native';
 export const colors = {
     green: '#8bc34a',
     blue: '#007aff',
-    disabled: '#dedede'
+    disabled: '#dedede',
+    red: 'red',
+    black: '#000'
+
 }
 export const styles = StyleSheet.create({
     container: {
@@ -31,6 +34,12 @@ export const styles = StyleSheet.create({
         borderRadius: 5,
         width: 250
     },
+    quizButton: { 
+        padding: 10,
+        alignItems: 'center',
+        alignSelf: 'center', 
+        width: 150,
+    },
     deleteButton:{
         position: 'absolute',
         bottom: 0
@@ -48,5 +57,34 @@ export const styles = StyleSheet.create({
         padding: 20,
         borderBottomWidth: 1,
         borderBottomColor: '#dedede'
+    },
+    viewPager: {
+        flex: 1,
+        borderWidth: 1,
+        width: Dimensions.get('window').width,
+        height: "50%"
+    },
+    page: {
+      alignItems: 'center',
+      justifyContent: 'center'  
+    },
+
+    questionCard: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        width: 300,
+        alignItems: 'center',
+        backgroundColor: '#eef', 
+        height: 200,
+        padding: 10,
+    },
+    question: {
+        fontSize: 18,
+        marginTop: 40
+    },
+    buttonView: {
+        flexDirection: 'row', 
+        marginTop: 40, 
     }
+    
 });
